11fe019f4b28de450552338f958e6f858e1352da
YuanXMjoy <850974686@qq.com>
2016-10-03 11:05:38 +0800

    reorganize the login page

--- Dian/js/join_css.js
+++ Dian/js/join_css.js
@@ -1,3 +1,23 @@
 /**
  * Created by JOYyuan on 16/10/2.
  */
+$(document).ready(function () {
+    statusSelect();
+    $("#select1").attr("class", "selected");
+    $(".registerBox").hide();
+
+});
+function statusSelect() {
+    $("#select1").click(function () {
+        $("#select1").attr("class", "selected");
+        $("#select2").removeAttr("class","selected");
+        $(".loginBox").show();
+        $(".registerBox").hide();
+    });
+    $("#select2").click(function () {
+        $("#select2").attr("class", "selected");
+        $("#select1").removeAttr("class","selected");
+        $(".loginBox").hide();
+        $(".registerBox").show();
+    });
+}
