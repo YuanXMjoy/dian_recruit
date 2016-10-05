adbc413b5542d67c84a076aabc850fba975ddab7
YuanXMjoy <850974686@qq.com>
2016-10-05 20:48:38 +0800

    complete the index function

---
+++ Dian/js/menubar_css.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".menu li").mouseover(function(){
+        $(this).css("border-bottom","1px solid #FA8823");
+    });
+    $(".menu li").mouseout(function(){
+        $(this).css("borderStyle","none");
+    });     
+}); 
