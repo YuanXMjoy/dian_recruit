7f9e99f1d39ebe46347a834b075ab5629c8da4b4
tcxxxx <luffyrocks@sina.com>
2016-11-02 19:00:33 +0800

    fix text positions in loggedIn page

--- Dian/js/menubar_css.js
+++ Dian/js/menubar_css.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
     $(".menu li").mouseout(function(){
         $(this).css("borderStyle","none");
     });   
+    $("#submit").mousedown(function(){
+    	$(this).css("box-shadow","0 0 0")
+    		   .css("left","586px")
+    		   .css("top","2px")
+    });
+    $("#submit").mouseup(function(){
+    	$(this).css("box-shadow","2px 2px 3px #B35914")
+    		   .css("left","588px")
+    		   .css("top","0")
+    });
 }); 
