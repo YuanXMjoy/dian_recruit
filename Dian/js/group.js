3894a18969c7cfca479902b3f1879e792f95b35a
tcxxxx <luffyrocks@sina.com>
2016-11-02 23:39:08 +0800

    some style changes on the group page

--- Dian/js/group.js
+++ Dian/js/group.js
@@ -4,3 +4,32 @@
 $(function(){
     $('#dowebok').fullpage();
 });
+
+$(document).ready(function(){
+	$("body").mouseover(function(){
+		$("#stick").hide();
+		$("#contactD").css("position","relative")
+					  .css("top","-66px")
+		$("#tm").css("font-size","14px")
+				.css("margin-top","10px");
+		$("#contact_tel")
+						.css("position","relative")
+						.css("top","-66px")
+						.css("padding-bottom","12px")
+						.css("padding-top","12px")
+		$("#feedback_mail")
+						.css("position","relative")
+						.css("top","-66px")		
+						.css("font-size","14px")
+						.css("padding-bottom","12px");						
+		$("#qgroup")
+					.css("position","relative")
+					.css("top","-66px")		
+					.css("font-size","14px")
+					.css("margin","0")
+					.css("padding-bottom","12px");	
+		$("#qrcode")
+					.css("position","relative")
+					.css("top","-66px")						
+	})
+});
