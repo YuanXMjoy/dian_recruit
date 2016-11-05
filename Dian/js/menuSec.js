d80ce749f7df3f2d68ab68510e051e764293f0ad
YuanXMjoy <850974686@qq.com>
2016-11-06 07:45:52 +0800

    add menu click js

---
+++ Dian/js/menuSec.js
@@ -0,0 +1,20 @@
+/**
+ * Created by JOYyuan on 16/11/6.
+ */
+$(document).ready(function(){
+    $("#m_index").click(function(){
+        window.location.href="index.html"
+    });
+    $("#m_groups").click(function(){
+        window.location.href="groups.html";
+    });
+    $("#m_proc").click(function(){
+        window.location.href="process.html";
+    });
+    $("#m_dian").click(function(){
+        window.open('http://dian.hust.edu.cn','_blank');
+    });
+    $("#m_join").click(function(){
+        window.location.href="arrange.html";
+    });
+});
