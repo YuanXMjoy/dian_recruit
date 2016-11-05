96a029a35e0e7c437cc5d6cf743d47bdf28ccd63
YuanXMjoy <850974686@qq.com>
2016-11-05 16:35:36 +0800

    add the pre information to the apply form

--- Dian/js/arrange.js
+++ Dian/js/arrange.js
@@ -1,7 +1,7 @@
 /**
  * Created by JOYyuan on 16/11/2.
  */
-    //这样应该以电话号码为判断看是否登录过，没有登录的话跳转到登录页登录了话就再说
+//这样应该以电话号码为判断看是否登录过，没有登录的话跳转到登录页登录了话就再说
 var phone = sessionStorage.user;
 var uid = sessionStorage.uid;
 
@@ -21,12 +21,15 @@ var prize;
 $(document).ready(function () {
     $("#userP").html(phone);
     applyInfo();
+
 });
 function applyInfo() {
     $("#applyBtn").click(function () {
         window.location.href = "loggedIn.html";
     });
-    if (uid != "") {
+    if (uid == undefined) {
+
+    }else{
         getInfo(phone);
     }
 }
@@ -34,20 +37,53 @@ function getInfo(num) {
     $.post("http://120.76.117.125:90/user/getuserbyph", {
         "phone": num
     }, function (res) {
-        var resObj = JSON.parse(num);
-        stuId=resObj.uid;
-        applyName=resObj.name;
-        applyPh=resObj.phone;
-        level=resObj.level;
-        gpa=resObj.score;
-        failC=resObj.fail_course;
-        tech=resObj.skills;
-        cv=resObj.features;
-        plan=resObj.plan;
-        sex=resObj.sex;
-        apEmail=resObj.email;
-        apMajor=resObj.major;
-        prize=resObj.champion;
-        //现在还要将那些信息插入到表格中；注意年纪和性别和是否挂科应该遍历，注意你拿来的东西可能是字符串不是数字记得转换。
+        var resObj = JSON.parse(res);
+        stuId = resObj.uid;
+        applyName = resObj.name;
+        applyPh = resObj.phone;
+        level = resObj.level;
+        gpa = resObj.score;
+        failC = resObj.fail_course;
+        tech = resObj.skills;
+        cv = resObj.features;
+        plan = resObj.plan;
+        sex = resObj.sex;
+        apEmail = resObj.email;
+        apMajor = resObj.major;
+        prize = resObj.champion;
+        putInfo(stuId,applyName,applyPh,level,gpa,failC,tech,cv,plan,sex,apEmail,apMajor,prize);
+
     });
+
+}
+function putInfo(stuId, apName, apPh, level, gpa, failC, tech, cv, plan, sex, apEmail, apMajor, prize) {
+    $("#input_name").val(apName);
+    $("#input_uid").val(stuId);
+    $("#input_tel").val(apPh);
+    $("#input_mail").val(apEmail);
+    $("#input_major").val(apMajor);
+    $("#input_gpa").val(gpa);
+    $("#input_prizes").val(prize);
+    $("#input_tech").val(tech);
+    $("#input_intro").val(cv);
+    $("#input_join").val(plan);
+    var sexChecks = document.getElementsByName("sex");
+    for (var i = 0; i < sexChecks.length; i++) {
+        if (i == sex) {
+            $(sexChecks[i]).attr('checked', 'checked');
+        }
+    }
+    var grades = document.getElementsByName("grade");
+    for (var k = 0; k < grades.length; k++) {
+        if (grades[k].value == level) {
+            $(grades[k]).attr('checked', 'checked');
+        }
+    }
+    var fails = document.getElementsByName("failure");
+    for (var j = 0; j < fails.length; j++) {
+        if (j == failC) {
+            $(fails[j]).attr('checked', 'checked');
+        }
+    }
+
 }
